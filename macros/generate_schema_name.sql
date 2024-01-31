21f333e49d609e7a3c618cce0f033e99b1008f63
Treven Mull <trevendevelopmentpractice@gmail.com>
2024-01-31 22:28:25 +0000

    Typo in generate_schema_name macro

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,5 +1,5 @@
 {% macro generate_schema_name(custom_schema_name, node) %}
-    {%- set default_schema = target.schema - %}
+    {%- set default_schema = target.schema -%}
     {%- if custom_schema_name is none -%}
         {{ default_schema }}
     {%- else -%}
